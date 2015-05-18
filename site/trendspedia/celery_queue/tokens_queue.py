23f5c182d02e52dc379813f79c04d3ec510c2464
Scarlett <scarlett.dongyan@gmail.com>
2015-05-18 16:07:06 +0800

    remove inactivate tokens when refreshing token queues

--- site/trendspedia/celery_queue/tokens_queue.py
+++ site/trendspedia/celery_queue/tokens_queue.py
@@ -109,6 +109,20 @@ class TokensQueue:
         # Store tokens in a list
         for user in ua:
             if (user.provider == "twitter"):
+                #DY
+                from django.conf import settings
+                import requests                
+
+                print "tokens_queue.py refreshPQ begin"
+                url = u'https://api.twitter.com/1.1/account/verify_credentials.json'
+                from requests_oauthlib import OAuth1
+                oauth = OAuth1(unicode(settings.TWITTER_CONSUMER_KEY), unicode(settings.TWITTER_CONSUMER_SECRET),
+                        unicode(user.tokens['oauth_token']), unicode(user.tokens['oauth_token_secret']), signature_type='query')
+                response = requests.get(url, auth=oauth)
+                if (response.status_code == 401):
+                    #remove invalid tokens
+                    user.delete()
+                
                 self.tokens.append(user.tokens)
 
         self.initiatePQ(self.tokens)
