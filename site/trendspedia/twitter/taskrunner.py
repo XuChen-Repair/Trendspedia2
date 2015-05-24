7161a50d21532862dec0fdba740d1aab08e9a648
Allwin Panikulam <allwinpb@gmail.com>
2015-05-24 17:01:37 +0800

    Timing data introduced for taskrunner

--- site/trendspedia/twitter/taskrunner.py
+++ site/trendspedia/twitter/taskrunner.py
@@ -9,15 +9,20 @@ import locale
 import breadability.readable as readable
 from bs4 import BeautifulSoup
 
+from time import time
+
 app = Celery('urls')
 
 @app.task
 def summarize(id):
+  start_time = time()
   page = Hot.objects(pk=id).first()
+  retrieve_time = time()
   if page is None:
     return
   url = page.url
-  page.crawled, page.url, page.title, page.description, page.images = extractContentFromUrl(url)
+  page.crawled, summ_time, page.url, page.title, page.description, page.images = extractContentFromUrl(url)
+  crawl_time = time()
   duplicate = Hot.objects(pk__ne=id, url=page.url, crawled=True)
   if len(duplicate) == 0 and page.crawled == True:
     # No duplicate URLs and no errors in parsing, save the page
@@ -29,7 +34,14 @@ def summarize(id):
   elif page.crawled == False:
     # Errors in parsing (or exceptions etc..)
     page.delete()
-
+  duplicate_time = time()
+  total_time = duplicate_time - start_time
+  duplicate_time = duplicate_time - crawl_time
+  crawl_time = crawl_time - retrieve_time
+  crawl_time = crawl_time - summ_time
+  retrieve_time = retrieve_time - start_time
+  if page.crawled:
+    print ":>>{0}, {1}, {2}, {3}, {4}".format(total_time, retrieve_time, crawl_time, summ_time, duplicate_time)
 
 def extractContentFromUrl(url):
   """
@@ -56,9 +68,17 @@ def extractContentFromUrl(url):
     print 'BadStatusLine:\t' + url
   except:
     print url, sys.exc_info()[0]
-  document = readable.Article(content, url=url, return_fragment=False)
-  encoding = locale.getpreferredencoding()
-  reduced_content = document.readable.encode(encoding)
+  reduced_content = ""
+  crawled = False
+  try:
+    start_time = time()
+    document = readable.Article(content, url=url, return_fragment=False)
+    encoding = locale.getpreferredencoding()
+    reduced_content = document.readable.encode(encoding)
+    crawled = True
+    summ_time = time() - start_time
+  except:
+    print url, sys.exc_info()[0]
   title = ""
   description = ""
   images = []
@@ -71,4 +91,4 @@ def extractContentFromUrl(url):
       title = bigdom.title.string.strip()
     description = dom.get_text()
     images = map(lambda img: img.get('src'), dom.find_all('img'))
-  return crawled, url, title, description, images
+  return crawled, summ_time, url, title, description, images
